ac6e0cbc97eb4b3f76b67d17c9a499ec738d6656
nickson.mwangi <nickson.mwangi@sovtech.com>
2022-11-16 15:50:04 +0300

    initial commit

---
+++ CSharpMultipleTasks/MultipleTasks/Cars.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMultipleTasks.MultipleTasks
+{
+    public class Cars
+    {
+        public string Registration_No { get; set; } = string.Empty;
+        public string Chasis_No { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+
+        public static List<Cars> GenerateCars(int number, char val)
+        {
+            List<Cars> cars = new List<Cars>();
+            for (int n = 0; n < number; n++)
+            {
+                val++;
+                Cars car = new Cars()
+                {
+                    Registration_No = string.Concat("KCY", number,n, val),
+                    Chasis_No = string.Concat("NSP-OOK",n, val, "-", number),
+                    Make = "Volkswagen"
+                };
+                cars.Add(car);
+            }
+            return cars;
+        }
+
+        /* TODO 
+         * Process each car 
+         */
+        public static async Task<string> ProcessCar(Cars car)
+        {
+            //Delaying execution for 1 Sec to help test task.whenAll
+            await Task.Delay(1000);
+            string message = $"Car Number: {car.Registration_No} Make: {car.Make} Processed";
+            //Console.WriteLine($"Car Number: {car.Registration_No} Processed");
+            return message;
+        }
+
+        /* TODO 
+         * Execute the Multiple tasks concurrrently with 
+         * foreach
+         * await Task.WhenAll(tasks): Tells there is a list of tasks and should wait until all tasks are done before continuing. 
+         * await Task.WhenAll(tasks): Enables all tasks to be run simultaneously.
+         * A problem with this is So, until the await Task.WhenAll(tasks) statement is called, our Main thread is frozen.
+         * As soon as we called await Task.WhenAll(tasks) method, the thread is active and starts processing.
+         */
+        public static async void ProcessCarsWithTaskWhenAll(List<Cars> cars)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var tasks = new List<Task<string>>();
+            foreach (var car in cars)
+            {
+                var response = ProcessCar(car);
+                tasks.Add(response);
+            }
+
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+            Console.WriteLine($"Processing with TaskWhenAll of {cars.Count} Car Done in {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+        }
+
+
+        /* TODO 
+         * Normal methos Task.WhenAll();
+         */
+        public static async void ProcessCarsWithoutTaskWhenAll(List<Cars> cars)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var tasks = new List<Task<string>>();
+            foreach (var car in cars)
+            {
+                var response = await ProcessCar(car);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Processing without TaskWhenAll of {cars.Count} Car Done in {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+        }
+
+        /* TODO
+         * Offloading the Current Thread
+         * don’t want our Main thread to freeze for 8 seconds
+         * offload the foreach loop to another thread by using the Task.Run Asynchronous Method in C#
+         * 
+         */
+        public static async void ProcessCarsOffLoad(List<Cars> cars)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var tasks = new List<Task<string>>();
+
+            await Task.Run(() =>
+            {
+                foreach (var car in cars)
+                {
+                    var response = ProcessCar(car);
+                    tasks.Add(response);
+                }
+            });
+            //It will execute all the tasks concurrently
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+            Console.WriteLine($"Processing with Task.Run of {cars.Count} Car Done in {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+        }
+    }
+}
---
+++ CSharpMultipleTasks/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using CSharpMultipleTasks.MultipleTasks;
+using System.Diagnostics;
+
+/* TODO
+ * Event with task.delay(1000) of 1 second it completes with 1.07secs
+ * If we remove Task.Delay(1000) in ProcessCar it will take 0 seconds
+ * this is because Task.WhenAll(tasks) executes all the tasks concurrently which drastically improves the performance
+ */
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+Console.WriteLine($"Main Thread Started");
+List<Cars> cars = Cars.GenerateCars(10, 'A');
+Console.WriteLine($"Cars Generated : {cars.Count}");
+/* TODO
+ * with Task.WhenAll(tasks) and Task.Delay(1000) takes 1.06 seconds 
+ * with Task.WhenAll(tasks) and without Task.Delay(1000) takes 0.001 seconds 
+ * The Problem with is, It makes the main thred freeze for 8 seconds
+ */
+Cars.ProcessCarsWithTaskWhenAll(cars);
+
+
+/* TODO
+ * without Task.WhenAll(tasks) and Task.Delay(1000) takes 10.14 seconds 
+ * without Task.WhenAll(tasks) and without Task.Delay(1000) takes 0.00 seconds 
+ */
+Cars.ProcessCarsWithoutTaskWhenAll(cars);
+
+
+/*
+ * with this main thread doesn't freeze anymore.
+ *This reduces main thread execution time
+ */
+Cars.ProcessCarsOffLoad(cars);
+
+
+Console.WriteLine($"Main Thread Completed");
+stopwatch.Stop();
+Console.WriteLine($"Main Thread Execution Time {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+Console.ReadKey();
